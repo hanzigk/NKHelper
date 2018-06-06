5ef17e937d8dacfa865e590606aeee973b37f574
SamanthaZeng <693487909@qq.com>
2018-06-06 22:49:25 +0800

    111

--- client/pages/hot/hotdetail/hotdetail.js
+++ client/pages/hot/hotdetail/hotdetail.js
@@ -10,25 +10,37 @@ Page({
       var temp=this.data.mid
       var that = this;
       wx.request({
-        url: 'http://10.134.39.81:3000/searchByOrderPutID?OrderPut_ID='+temp,//此处填写你后台请求地址
+        url: 'http://10.134.39.81:3000/searchByOrderPutID?OrderPut_ID=' + options.id,//此处填写你后台请求地址
         header: {
           'content-type': 'application/json' // 默认值
         },
         success: function (res) {
           console.log(res.data);    
           that.setData({
-            ordername: res.data.Order_Title,
-            sendername: res.data.Wechat_Number_Put,
-            Order_Type: res.data.Order_Type,
-            content: res.data.Order_Content,
-            id: res.data.OrderPut_ID,
-            time: res.data.Order_Time,
-            Order_MaxNumber: res.data.Order_MaxNumber,
-            Order_NowNumber: res.data.Order_NowNumber
+            ordername: res.data[0].Order_Title,
+            sendername: res.data[0].Wechat_Number_Put,
+            Order_Type: res.data[0].Order_Type,
+            content: res.data[0].Order_Content,
+            id: res.data[0].OrderPut_ID,
+            time: res.data[0].Order_Time,
+            Order_MaxNumber: res.data[0].Order_MaxNumber,
+            Order_NowNumber: res.data[0].Order_NowNumber
           });
         }
       })
-    
+  },
+  addorder:function(event)
+  {
+    var that = this;
+    wx.request({
+      url: 'http://10.134.39.81:3000/addOrderGet?OrderGet_ID='+this.data.id+'&Wechat_Number_Get=chenxingqiming',//此处填写你后台请求地址
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data);
+      }
+    })
   },
   data:{
         id: 0,
--- client/pages/user/myActivity/myActivityDetail/myActivityDetail.js
+++ client/pages/user/myActivity/myActivityDetail/myActivityDetail.js
@@ -1,66 +1,87 @@
-// pages/user/myActivity/myActivityDetail/myActivityDetail.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-  
+    console.log(options.id);
+    wx.setNavigationBarTitle({
+      title: '订单详情'
+    })
+    this.setData({
+      mid: options.id,
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  data: {
+    sendorderdetail: [
+      {
+        ordername: "coco",
+        price: "12",
+        orderstatus: "已成交",
+        sendername: "A",
+        receivername: "B",
+        id: "1",
+        content: "第一杯"
+      },
+      {
+        ordername: "coco",
+        price: "13",
+        orderstatus: "已成交",
+        sendername: "A",
+        receivername: "B",
+        id: "2",
+        content: "第二杯"
+      },
+      {
+        ordername: "coco",
+        price: "14",
+        orderstatus: "已成交",
+        sendername: "A",
+        receivername: "B",
+        id: "3",
+        content: "第三杯"
+      },
+      {
+        ordername: "coco",
+        price: "15",
+        orderstatus: "已成交",
+        sendername: "A",
+        receivername: "B",
+        id: "4",
+        content: "第四杯"
+      },
+      {
+        ordername: "coco",
+        price: "16",
+        orderstatus: "已成交",
+        sendername: "A",
+        receivername: "B",
+        id: "5",
+        content: "第五杯"
+      },
+      {
+        ordername: "coco",
+        price: "17",
+        orderstatus: "已成交",
+        sendername: "A",
+        receivername: "B",
+        id: "6",
+        content: "第六杯"
+      },
+      {
+        ordername: "coco",
+        price: "18",
+        orderstatus: "已成交",
+        sendername: "A",
+        receivername: "B",
+        id: "7",
+        content: "第七杯"
+      },
+      {
+        ordername: "coco",
+        price: "19",
+        orderstatus: "已成交",
+        sendername: "A",
+        receivername: "B",
+        id: "8",
+        content: "第八杯"
+      }
+    ]
   }
-})
+})  
