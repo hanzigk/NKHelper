96b7b58c3f25e21efbd793a980bb62c04c211b5d
辰星启明 <2547979215@qq.com>
2018-06-08 11:51:29 +0800

    13

--- client/pages/hot/hotdetail/hotdetail.js
+++ client/pages/hot/hotdetail/hotdetail.js
@@ -32,23 +32,23 @@ Page({
   addorder:function(event)
   {
     var that = this;
-    var temp=-1;
-    wx.checkSession({
-      success: function () {
-        console.log("判断会话")
-        //session_key 未过期，并且在本生命周期一直有效
-        temp=1;
-      },
-      fail: function () {
-        // session_key 已经失效，需要重新执行登录流程
+    if (getApp().globalData.Wechat_Number==""){
         wx.showToast({
-          title: '您还未登陆！请先前往个人中心登陆',
+          title: '您还未登陆！请先登陆',
+          icon: 'none',
+          duration: 1500,
+          success: function () {
+            setTimeout(function () {
+              wx.switchTab({
+                url: '/pages/user/user'
+              })
+            }, 1500)
+          }
         })
-     }
-    })
-    if(temp==1){
+        return;
+    }
     wx.request({
-      url: 'http://10.134.39.81:3000/addOrderGet?OrderGet_ID='+this.data.id+'&Wechat_Number_Get=chenxingqiming',//此处填写你后台请求地址
+      url: 'http://10.134.39.81:3000/addOrderGet?OrderGet_ID=' + this.data.id + '&Wechat_Number_Get=' + getApp().globalData.Wechat_Number,//此处填写你后台请求地址
       header: {
         'content-type': 'application/json' // 默认值
       },
@@ -57,25 +57,49 @@ Page({
         {
           wx.showToast({
             title: '您已接过该任务',
+            icon: 'none',
+            duration: 1500
           })
+          return;
         } if (res.data == "已接任务数量已满") {
           wx.showToast({
             title: '您已接了3个任务，需完成后再接',
+            icon: 'none',
+            duration: 1500
           })
+          return;
         }
         if (res.data == "此人为任务发布人") {
           wx.showToast({
             title: '您不能接自己发布的任务',
+            icon: 'none',
+            duration: 1500
           })
+          return;
         }
         if (res.data == "任务所需人数已满") {
           wx.showToast({
             title: '该任务所需人数已满',
+            icon: 'none',
+            duration: 1500
           })
+          return;
         }
         console.log(res.data);
+        wx.showToast({
+          title: '接单成功',
+          icon: 'success',
+          duration: 1500,
+          success: function () {
+            setTimeout(function () {
+              wx.switchTab({
+                url: '/pages/user/user'
+              })
+            }, 1500)
+          }
+        })
       }
-    })}
+    })
   },
   data:{
         id: 0,
